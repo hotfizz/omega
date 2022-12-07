acd956a8f64307cee064a4ab7d9091fdc93a0344
xkkhy <784457618@qq.com>
2022-12-07 23:26:27 +0800

    [mod] refactor

--- alt/normalize.go
+++ alt/normalize.go
@@ -115,110 +115,180 @@ func (c *dataEtl) normalize(
 		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
 		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
 		reflect.Bool, reflect.Uintptr, reflect.Float32, reflect.Float64, reflect.String:
-		tmp := cpm(currentMap)
-		tmp[prefix] = data
-		c.logger.Debug(fmt.Sprintf("current type is %v, value is %v", reflect.TypeOf(data).Kind(), data))
-		return append(result, tmp), err
+
+		return c.parsePrimitive(data, prefix, currentMap, depth, err)
+
 	case reflect.Slice:
-		var newList []map[string]interface{}
-		var s = reflect.ValueOf(data)
-		c.logger.Debug(fmt.Sprintf("type is slice len is %d", s.Len()))
-		for i := 0; i < s.Len(); i++ {
-			var _res, tmpErr = c.normalize(s.Index(i).Interface(), prefix, cpm(currentMap), depth+1, err)
-			newList = append(newList, _res...)
-			err = append(err, tmpErr...)
-		}
-		// 如果列表为空, 返回 currentMap 对象本身的数组
-		if len(newList) == 0 {
-			newList = append(newList, cpm(currentMap))
-		}
-		return newList, err
+
+		return c.parseSlice(data, prefix, currentMap, depth, err)
+
 	case reflect.Map:
-		c.logger.Debug("type is map ", data)
-		var newList = []map[string]interface{}{cpm(currentMap)}
-		for mr := reflect.ValueOf(data).MapRange(); mr.Next(); {
-			_key := mr.Key().Interface()
-			_v := mr.Value().Interface()
-			c.logger.Debug(fmt.Sprintf("rec map key:%v, value:%v", _key, _v))
-			// NOTE 必须保证判断是有效的
-			// this case { "data": null }
-			if _v == nil {
-				c.logger.Warn(_key, " nil type ")
-				continue
-			}
 
-			switch reflect.TypeOf(_v).Kind() {
-			case
-				reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
-				reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
-				reflect.Bool, reflect.Uintptr, reflect.Float32, reflect.Float64, reflect.String:
-				// 如果当前的值 数值，整型，布尔时，填充到所有已经遍历的对象
-				for i := range newList {
-					newList[i][c.separator.AppendToPrefix(prefix, _key)] = _v
-				}
-			case reflect.Map, reflect.Slice, reflect.Struct:
-				var copyList = make([]map[string]interface{}, 0)
-				c.logger.Debug(fmt.Sprintf("type is %v", reflect.TypeOf(_v)))
-				for _, _v2 := range newList {
-					var _res, tmpErr = c.normalize(_v, c.separator.AppendToPrefix(prefix, _key), _v2, depth+1, err)
-					copyList = append(copyList, _res...)
-					err = append(err, tmpErr...)
-				}
-				newList = copyList
-			default:
-				c.logger.Warn("unknown type ", reflect.TypeOf(_v).Kind())
-			}
-		}
-		return newList, err
+		return c.parseMap(data, prefix, currentMap, depth, err)
+
 	case reflect.Struct:
-		c.logger.Debug("type is struct  ", data)
-		var newList = []map[string]interface{}{cpm(currentMap)}
-		rv := reflect.ValueOf(data)
-		rt := reflect.TypeOf(data)
-		for i := 0; i < rv.NumField(); i++ {
-			_key := rt.Field(i).Name
-			_v := rv.Field(i).Interface()
-			c.logger.Debug(fmt.Sprintf("rec map key:%v, value:%v", _key, _v))
-			// NOTE 必须保证判断是有效的
-			// this case { "data": null }
-			if _v == nil {
-				c.logger.Warn(_key, " nil type ")
-				continue
-			}
 
-			switch reflect.TypeOf(_v).Kind() {
-			case
-				reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
-				reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
-				reflect.Bool, reflect.Uintptr, reflect.Float32, reflect.Float64, reflect.String:
-				// 如果当前的值 数值，整型，布尔时，填充到所有已经遍历的对象
-				for i := range newList {
-					newList[i][c.separator.AppendToPrefix(prefix, _key)] = _v
-				}
-			case reflect.Map, reflect.Slice, reflect.Struct:
-				var copyList = make([]map[string]interface{}, 0)
-				c.logger.Debug(fmt.Sprintf("type is %v", reflect.TypeOf(_v)))
-				for _, _v2 := range newList {
-					var _res, tmpErr = c.normalize(_v, c.separator.AppendToPrefix(prefix, _key), _v2, depth+1, err)
-					copyList = append(copyList, _res...)
-					err = append(err, tmpErr...)
-				}
-				newList = copyList
-				// 不支持的类型
-			case reflect.Chan, reflect.Pointer, reflect.Func, reflect.Complex64, reflect.Complex128, reflect.UnsafePointer:
-				c.logger.Warn("unknown type i ", reflect.TypeOf(_v).Kind())
-			default:
-				c.logger.Warn("unknown type ", reflect.TypeOf(_v).Kind())
-			}
-		}
-		return newList, err
+		return c.parseStruct(data, prefix, currentMap, depth, err)
 
 	case reflect.Chan, reflect.Pointer, reflect.Func, reflect.Complex64, reflect.Complex128, reflect.UnsafePointer:
+
 		c.logger.Warn("unknown type i ", reflect.TypeOf(data).Kind())
+
 	default:
+
 		c.logger.Warn("unknown type ", reflect.TypeOf(data).Kind())
 	}
+
 	return []map[string]interface{}{cpm(currentMap)}, err
+}
+
+// primitive  判断是否为基础类型
+func (c *dataEtl) primitive(k reflect.Kind) bool {
+	switch k {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Bool, reflect.Uintptr, reflect.Float32, reflect.Float64, reflect.String:
+		return true
+	default:
+		return false
+	}
+}
+
+// 处理基础类型
+func (c *dataEtl) parsePrimitive(
+	data interface{},
+	prefix string,
+	currentMap map[string]interface{},
+	depth int,
+	err []error,
+) (result []map[string]interface{}, resErr []error) {
+	if !c.primitive(reflect.TypeOf(data).Kind()) {
+		// 做点什么
+	}
+	tmp := cpm(currentMap)
+	tmp[prefix] = data
+	c.logger.Debug(fmt.Sprintf("current type is %v, value is %v", reflect.TypeOf(data).Kind(), data))
+	return append(result, tmp), err
+}
+
+// 处理切片类型
+func (c *dataEtl) parseSlice(
+	data interface{},
+	prefix string,
+	currentMap map[string]interface{},
+	depth int,
+	err []error,
+) (result []map[string]interface{}, resErr []error) {
+	var newList []map[string]interface{}
+	var s = reflect.ValueOf(data)
+	c.logger.Debug(fmt.Sprintf("type is slice len is %d", s.Len()))
+	for i := 0; i < s.Len(); i++ {
+		var _res, tmpErr = c.normalize(s.Index(i).Interface(), prefix, cpm(currentMap), depth+1, err)
+		newList = append(newList, _res...)
+		err = append(err, tmpErr...)
+	}
+	// 如果列表为空, 返回 currentMap 对象本身的数组
+	if len(newList) == 0 {
+		newList = append(newList, cpm(currentMap))
+	}
+	return newList, err
+}
+
+// 解析 map 类型
+func (c *dataEtl) parseMap(
+	data interface{},
+	prefix string,
+	currentMap map[string]interface{},
+	depth int,
+	err []error,
+) (result []map[string]interface{}, resErr []error) {
+	c.logger.Debug("type is map ", data)
+	var newList = []map[string]interface{}{cpm(currentMap)}
+	for mr := reflect.ValueOf(data).MapRange(); mr.Next(); {
+		_key := mr.Key().Interface()
+		_v := mr.Value().Interface()
+		c.logger.Debug(fmt.Sprintf("rec map key:%v, value:%v", _key, _v))
+		// NOTE 必须保证判断是有效的
+		// this case { "data": null }
+		if _v == nil {
+			c.logger.Warn(_key, " nil type ")
+			continue
+		}
+
+		switch reflect.TypeOf(_v).Kind() {
+		case
+			reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+			reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+			reflect.Bool, reflect.Uintptr, reflect.Float32, reflect.Float64, reflect.String:
+			// 如果当前的值 数值，整型，布尔时，填充到所有已经遍历的对象
+			for i := range newList {
+				newList[i][c.separator.AppendToPrefix(prefix, _key)] = _v
+			}
+		case reflect.Map, reflect.Slice, reflect.Struct:
+			var copyList = make([]map[string]interface{}, 0)
+			c.logger.Debug(fmt.Sprintf("type is %v", reflect.TypeOf(_v)))
+			for _, _v2 := range newList {
+				var _res, tmpErr = c.normalize(_v, c.separator.AppendToPrefix(prefix, _key), _v2, depth+1, err)
+				copyList = append(copyList, _res...)
+				err = append(err, tmpErr...)
+			}
+			newList = copyList
+		default:
+			c.logger.Warn("unknown type ", reflect.TypeOf(_v).Kind())
+		}
+	}
+	return newList, err
+}
+
+// 解析 struct 类型
+func (c *dataEtl) parseStruct(
+	data interface{},
+	prefix string,
+	currentMap map[string]interface{},
+	depth int,
+	err []error,
+) (result []map[string]interface{}, resErr []error) {
+	c.logger.Debug("type is struct  ", data)
+	var newList = []map[string]interface{}{cpm(currentMap)}
+	rv := reflect.ValueOf(data)
+	rt := reflect.TypeOf(data)
+	for i := 0; i < rv.NumField(); i++ {
+		_key := rt.Field(i).Name
+		_v := rv.Field(i).Interface()
+		c.logger.Debug(fmt.Sprintf("rec map key:%v, value:%v", _key, _v))
+		// NOTE 必须保证判断是有效的
+		// this case { "data": null }
+		if _v == nil {
+			c.logger.Warn(_key, " nil type ")
+			continue
+		}
+
+		switch reflect.TypeOf(_v).Kind() {
+		case
+			reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+			reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+			reflect.Bool, reflect.Uintptr, reflect.Float32, reflect.Float64, reflect.String:
+			// 如果当前的值 数值，整型，布尔时，填充到所有已经遍历的对象
+			for i := range newList {
+				newList[i][c.separator.AppendToPrefix(prefix, _key)] = _v
+			}
+		case reflect.Map, reflect.Slice, reflect.Struct:
+			var copyList = make([]map[string]interface{}, 0)
+			c.logger.Debug(fmt.Sprintf("type is %v", reflect.TypeOf(_v)))
+			for _, _v2 := range newList {
+				var _res, tmpErr = c.normalize(_v, c.separator.AppendToPrefix(prefix, _key), _v2, depth+1, err)
+				copyList = append(copyList, _res...)
+				err = append(err, tmpErr...)
+			}
+			newList = copyList
+			// 不支持的类型
+		case reflect.Chan, reflect.Pointer, reflect.Func, reflect.Complex64, reflect.Complex128, reflect.UnsafePointer:
+			c.logger.Warn("unknown type i ", reflect.TypeOf(_v).Kind())
+		default:
+			c.logger.Warn("unknown type ", reflect.TypeOf(_v).Kind())
+		}
+	}
+	return newList, err
 }
 
 // copy map object
